820a47c089a1f2943bb4a57233a309bcbfe148b3
mwlaofr <millenaofrr@gmail.com>
2025-12-02 19:30:10 -0300

    feat: create favorite books screen with placeholder and empty message

--- app/(tabs)/favorite.tsx
+++ app/(tabs)/favorite.tsx
@@ -1,24 +1,61 @@
-import CustomButton from "@/components/CustomButton";
-import Textfield from "@/components/textfield";
-import { ThemedText } from "@/components/ThemedText";
-import { useRouter } from "expo-router";
-import { View } from "react-native";
+// app/(tabs)/favorite.tsx
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
-export default function TabFiveScreen() {
-  const router = useRouter();
+export default function FavoriteScreen() {
+  // exemplo de array (depois você troca pelo seu contexto/async storage/api)
+  const favorites = []; // coloque livros aqui
 
   return (
-    <View style={{ flex: 1, padding: 16 }}>
-      <ThemedText>history</ThemedText>
-
-      <Textfield placeholder="Usuário..." />
-      <Textfield placeholder="Senha..." secureTextEntry />
-      <CustomButton
-        title="Ir para Login"
-        variant="outline"
-        color="#007bff"
-        onPress={() => router.push("./index")}
-      />
+    <View style={styles.container}>
+      {favorites.length === 0 ? (
+        <View style={styles.emptyContainer}>
+          <Text style={styles.emptyText}>
+            Você ainda não tem livros favoritos
+          </Text>
+        </View>
+      ) : (
+        <FlatList
+          data={favorites}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.author}>{item.author}</Text>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  emptyContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  emptyText: {
+    fontSize: 16,
+    color: "#777",
+  },
+  card: {
+    padding: 16,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    marginBottom: 12,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  author: {
+    fontSize: 14,
+    color: "#555",
+  },
+});
--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.109.3.128:8080/api", 
+  baseURL: "http://10.109.3.234:8080/api", 
 });
 
 export default api;
